655487f89fa7b4b906d3ccb98524683c7d09230a
YaseminKasikci <102510030+YaseminKasikci@users.noreply.github.com>
2023-10-12 15:57:09 +0200

    Add files via upload

---
+++ PasAPas/NomVariables.swift
@@ -0,0 +1,27 @@
+//
+////RANDO
+//var imageCouvertureRando : String
+//var nomRando : String
+//var niveauRando :
+//var distance : Int
+//var durée : String
+//var denivele : Int
+//var descriptionRando : String
+//
+////PROFIL UTILISATEUR
+//var nomUtilisateur : String
+//var prenomUtilisateur : String
+//var bioUtilisateur : String
+//var ageUtilisateur : Int
+//var photoProfil : String
+//var profileVerifierUtilisateur : Bool
+//
+////PROFIL COPAINRANDO
+//
+//var nomCopain : String
+//var prenomCopain : String
+//var bioCopain : String
+//var ageCopain : Int
+//var photoCopain : String
+//var profileVerifierCopain : Bool
+//var dateRandoneeCopain : String
